7133cc24d918f74b07be8312c87099aaf3423f0f
ivandotv <390700+ivandotv@users.noreply.github.com>
2020-03-08 00:02:29 +0100

    improvement: make global declarations to be const instead of var

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,6 +1,2 @@
-// declare global {
-//   const __DEV__: boolean
-// }
-
-declare var __DEV__: boolean
-declare var __VERSION__: string
+declare const __DEV__: boolean
+declare const __VERSION__: string
