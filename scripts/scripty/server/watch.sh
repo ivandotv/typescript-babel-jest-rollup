11aaf1552e11faeb6f7138c4c248b7b83b15a705
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-01-16 00:31:06 +0100

    add git commit sha and time

--- scripts/scripty/server/watch.sh
+++ scripts/scripty/server/watch.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
 
-del dist/cjs && cross-env BABEL_ENV=cjs NODE_ENV=development babel src --out-dir dist/cjs --extensions \".ts,.tsx,js,jsx\" --source-maps --watch
+del dist/cjs && BABEL_ENV=cjs NODE_ENV=development babel src --out-dir dist/cjs --extensions \".ts,.tsx,js,jsx\" --source-maps --watch
