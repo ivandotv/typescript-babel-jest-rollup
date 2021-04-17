56aba2738eb86dc8b985c8384be30c536b8ff374
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-04-17 22:28:07 +0200

    fix prettier eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,8 +20,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'prettier',
-    'prettier/@typescript-eslint'
+    'prettier'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
